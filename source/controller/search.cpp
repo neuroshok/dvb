dce5cb2b9f70a554a820305bd94a9cd2fae01fa2
ads00 <ads.neuroshok@gmail.com>
2020-10-07 01:35:16 +0200

    [update] clean structure

--- source/controller/search.cpp
+++ source/controller/search.cpp
@@ -9,10 +9,10 @@ void search::process(const HttpRequestPtr &req,
     HttpViewData view_data;
 
     std::vector<std::string> resources;
-    view_data.insert("title","dvb title");
+    view_data.insert("title", input);
     resources.push_back("blablabla");
     resources.push_back("title_test2222222222222");
-    resources.push_back("blablablablablablablablablablablablablablablablablablablablablablablabla");
+    resources.push_back(input);
 
     view_data.insert("resources", resources);
     auto resp=HttpResponse::newHttpViewResponse("main.csp", view_data);
--- source/main.cpp
+++ source/main.cpp
@@ -2,8 +2,12 @@
 
 int main()
 {
-    drogon::app().loadConfigFile("config.json");
+    drogon::app().loadConfigFile("resource/config.json");
     drogon::app().run();
 
     return 0;
 }
+
+app/data/style
+app/view/
+app/controller
