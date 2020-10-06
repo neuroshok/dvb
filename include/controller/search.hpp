a46df48be2a912773dd4c6a241c7c014efb39945
ads00 <ads.neuroshok@gmail.com>
2020-10-07 00:13:14 +0200

    up

--- include/controller/search.hpp
+++ include/controller/search.hpp
@@ -3,21 +3,17 @@
 #include <drogon/HttpController.h>
 
 using namespace drogon;
-namespace dvb
+
+
+class search : public drogon::HttpController<search>
 {
-    namespace v1
-    {
-        class search : public drogon::HttpController<search>
-        {
-        public:
-            METHOD_LIST_BEGIN
-                //use METHOD_ADD to add your custom processing function here;
-                METHOD_ADD(search::process,"/{1}", Post);
-            METHOD_LIST_END
-            //your declaration of processing function maybe like this:
-            void process(const HttpRequestPtr &req,
-                       std::function<void (const HttpResponsePtr &)> &&callback,
-                       const std::string& input);
-        };
-    }
-}
+public:
+    METHOD_LIST_BEGIN
+        //use METHOD_ADD to add your custom processing function here;
+        METHOD_ADD(search::process,"/{1}", Post);
+    METHOD_LIST_END
+    //your declaration of processing function maybe like this:
+    void process(const HttpRequestPtr &req,
+                std::function<void (const HttpResponsePtr &)> &&callback,
+                const std::string& input);
+};
