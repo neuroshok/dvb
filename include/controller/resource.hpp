256e1ff00aeebde33d662417c5bf4cfe0e4d027b
ads00 <ads.neuroshok@gmail.com>
2020-10-07 22:37:56 +0200

    up

--- include/controller/resource.hpp
+++ include/controller/resource.hpp
@@ -11,8 +11,10 @@ class resource : public drogon::HttpController<resource>
 {
 public:
     METHOD_LIST_BEGIN
-        METHOD_ADD(resource::add,"/", drogon::Post);
+        METHOD_ADD(resource::view_add,"/add/", drogon::Get);
+        METHOD_ADD(resource::add,"/add/", drogon::Post);
     METHOD_LIST_END
 
-    void add(dvb::resource_data&&, std::function<void(const drogon::HttpResponsePtr &)>&&) const;
+    void view_add(const drogon::HttpRequestPtr&, std::function<void (const drogon::HttpResponsePtr&)>&&) const;
+    void add(dvb::resource_data&&, std::function<void(const drogon::HttpResponsePtr&)>&&) const;
 };
--- source/controller/resource.cpp
+++ source/controller/resource.cpp
@@ -1,12 +1,22 @@
 #include <controller/resource.hpp>
 #include <dvb/resource_data.hpp>
 
+void resource::view_add(const drogon::HttpRequestPtr& req, std::function<void (const drogon::HttpResponsePtr &)>&&) const
+{
+    drogon::HttpViewData view_data;
+
+    view_data.insert("title", "Add a new resource");
+    auto resp = drogon::HttpResponse::newHttpViewResponse("resource_add.csp", view_data);
+    callback(resp);
+}
+
 void resource::add(dvb::resource_data&& resource_data, std::function<void(const drogon::HttpResponsePtr &)>&& callback) const
 {
     drogon::HttpViewData view_data;
 
     std::vector<dvb::resource_data> resources{ std::move(resource_data) };
-    view_data.insert("resources", resources);
+    view_data.insert("title", "Add a new resource");
+    view_data.insert("status", "Resource has been added");
     auto resp = drogon::HttpResponse::newHttpViewResponse("resource_add.csp", view_data);
     callback(resp);
 }
