addd744a4c2e39eb7a72079781712a2f443ae470
ads00 <ads.neuroshok@gmail.com>
2020-10-07 22:01:28 +0200

    update views

---
+++ include/controller/resource.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <drogon/HttpController.h>
+
+namespace dvb
+{
+    struct resource_data;
+} // dvb
+
+class resource : public drogon::HttpController<resource>
+{
+public:
+    METHOD_LIST_BEGIN
+        METHOD_ADD(resource::add,"/", Post);
+    METHOD_LIST_END
+
+    void add(dvb::resource_data&&, std::function<void(const HttpResponsePtr &)>&&) const;
+
+namespace drogon
+{
+    template <>
+    inline dvb::resource_data fromRequest(const HttpRequest& req)
+    {
+        auto json = req.getJsonObject();
+        dvb::resource_data data;
+        if(json)
+        {
+            data.name = (*json)["name"].asString();
+            data.type = (*json)["type"].asString();
+            data.data = (*json)["data"].asString();
+        }
+        return data;
+    }
+};
---
+++ include/dvb/resource_data.hpp
@@ -0,0 +1,9 @@
+namespace dvb
+{
+    struct resource_data
+    {
+        std::string name;
+        std::string type;
+        std::string data;
+    };
+} // dvb
