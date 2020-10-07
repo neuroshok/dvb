5930a774bf40ea13352ec93c095d25e87b5e2fb5
ads00 <ads.neuroshok@gmail.com>
2020-10-07 22:13:04 +0200

    up

--- include/controller/resource.hpp
+++ include/controller/resource.hpp
@@ -11,7 +11,7 @@ class resource : public drogon::HttpController<resource>
 {
 public:
     METHOD_LIST_BEGIN
-        METHOD_ADD(resource::add,"/", Post);
+        METHOD_ADD(resource::add,"/", drogon::Post);
     METHOD_LIST_END
 
     void add(dvb::resource_data&&, std::function<void(const drogon::HttpResponsePtr &)>&&) const;
