fd4c67097e74be24b7516516218efe53ae227b4c
ads00 <ads.neuroshok@gmail.com>
2020-10-08 02:20:14 +0200

    up

--- source/controller/search.cpp
+++ source/controller/search.cpp
@@ -7,7 +7,8 @@ void search::process(const drogon::HttpRequestPtr &req,
     drogon::HttpViewData view_data;
 
     std::vector<std::string> resources;
-    view_data.insert("title", input);
+    view_data.insert("title", "Searching " + input);
+    view_data.insert("search_input", input);
     resources.push_back("blablabla");
     resources.push_back("title_test2222222222222");
     resources.push_back(input);
