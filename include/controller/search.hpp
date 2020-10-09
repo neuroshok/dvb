287fe677b63d0643a04fb74e705ef690a7f75c4b
ads00 <ads.neuroshok@gmail.com>
2020-10-09 03:11:15 +0200

    up

--- include/controller/search.hpp
+++ include/controller/search.hpp
@@ -2,15 +2,17 @@
 
 #include <drogon/HttpController.h>
 
-class search : public drogon::HttpController<search>
-{
-public:
-    METHOD_LIST_BEGIN
-        METHOD_ADD(search::process,"/?s={1}", drogon::Get);
-        METHOD_ADD(search::process,"/{1}", drogon::Get);
-    METHOD_LIST_END
+namespace web
+    class search : public drogon::HttpController<search>
+    {
+    public:
+        METHOD_LIST_BEGIN
+            METHOD_ADD(search::process,"/?s={1}", drogon::Get);
+            METHOD_ADD(search::process,"/{1}", drogon::Get);
+        METHOD_LIST_END
 
-    void process(const drogon::HttpRequestPtr&,
-                std::function<void (const drogon::HttpResponsePtr &)>&&,
-                const std::string& input);   
-};
+        void process(const drogon::HttpRequestPtr&,
+                    std::function<void (const drogon::HttpResponsePtr &)>&&,
+                    const std::string& input);   
+    };
+} // web
--- source/controller/search.cpp
+++ source/controller/search.cpp
@@ -1,19 +1,22 @@
 #include <controller/search.hpp>
 
-void search::process(const drogon::HttpRequestPtr &req,
-           std::function<void (const drogon::HttpResponsePtr &)> &&callback,
-           const std::string& input)
+namespace web
 {
-    drogon::HttpViewData view_data;
+    void search::process(const drogon::HttpRequestPtr &req,
+            std::function<void (const drogon::HttpResponsePtr &)> &&callback,
+            const std::string& input)
+    {
+        drogon::HttpViewData view_data;
 
-    std::vector<std::string> resources;
-    view_data.insert("title", "Searching " + input);
-    view_data.insert("search_input", input);
-    resources.push_back("blablabla");
-    resources.push_back("title_test2222222222222");
-    resources.push_back(input);
+        std::vector<std::string> resources;
+        view_data.insert("title", "Searching " + input);
+        view_data.insert("search_input", input);
+        resources.push_back("blablabla");
+        resources.push_back("title_test2222222222222");
+        resources.push_back(input);
 
-    view_data.insert("resources", resources);
-    auto resp= drogon::HttpResponse::newHttpViewResponse("search.csp", view_data);
-    callback(resp);
-}
+        view_data.insert("resources", resources);
+        auto resp= drogon::HttpResponse::newHttpViewResponse("search.csp", view_data);
+        callback(resp);
+    }
+} // web
