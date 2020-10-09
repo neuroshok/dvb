f3b7c500d2dac4ebed0bb9c703849d4d19981321
ads00 <ads.neuroshok@gmail.com>
2020-10-09 03:34:23 +0200

    up

--- source/controller/search.cpp
+++ source/controller/search.cpp
@@ -12,9 +12,9 @@ namespace web
         std::vector<dvb::resource_data> resources;
         view_data.insert("title", "Searching " + input);
         view_data.insert("search_input", input);
-        resources.emplace_back(dvb::resource_data("name", "type", "data"));
-        resources.emplace_back(dvb::resource_data(input, "type", "data"));
-        resources.emplace_back(dvb::resource_data("title_test2222222222222", "type", "data"));
+        resources.emplace_back(dvb::resource_data{ "name", "type", "data" });
+        resources.emplace_back(dvb::resource_data{ input, "type", "data" });
+        resources.emplace_back(dvb::resource_data{ "title_test2222222222222", "type", "data" });
 
         view_data.insert("resources", resources);
         auto resp= drogon::HttpResponse::newHttpViewResponse("search.csp", view_data);
