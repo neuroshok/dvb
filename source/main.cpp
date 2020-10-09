537a6ae0f1b920ef1614f472bd468f5bbdb56843
ads00 <ads.neuroshok@gmail.com>
2020-10-09 19:25:28 +0200

    up

--- source/main.cpp
+++ source/main.cpp
@@ -5,5 +5,24 @@ int main()
     drogon::app().loadConfigFile("resource/config.json");
     drogon::app().run();
 
+    auto clientPtr = drogon::app().getDbClient();
+
+    constexpr auto resource_table = R"(
+CREATE TABLE resource (
+    id int,
+    name varchar(255),
+    data text,
+);
+    )";
+
+    clientPtr->execSqlAsync(resource_table,
+                            [](const Result &r) {
+                                
+                            },
+                            [](const DrogonDbException& e) {
+                                std::cerr << "error:" << e.base().what() << std::endl;
+                            },
+                            "dvb");
+
     return 0;
 }
