8ba764a6553d08a657f5cbb45bf4630c7c414535
ads00 <ads.neuroshok@gmail.com>
2020-10-10 21:13:21 +0200

    up

--- source/main.cpp
+++ source/main.cpp
@@ -5,18 +5,6 @@ int main()
 {
     drogon::app().loadConfigFile("resource/config.json");
 
-    auto db = drogon::orm::DbClient:: newPgClient("default", 1);
-
-    constexpr auto create_table_resource = R"(
-CREATE TABLE resource (
-    id serial primary key,
-    name varchar(255),
-    data text,
-);
-    )";
-
-    db->execSqlSync(create_table_resource);
-
     drogon::app().run();
 
     return 0;
